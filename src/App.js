914dfdbae03c51cf2a74096325ddf63ddf0ba0f1
Damasy <super.nova.egy@gmail.com>
2020-03-27 23:24:59 +0200

    basica, conditionally show item and create lists

--- src/App.js
+++ src/App.js
@@ -1,26 +1,107 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Person from "./Person/Person";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    persons: [
+      { id: 'asddsa', name: "a.damasy", age: 27 },
+      { id: 'asdzxc', name: "Doc.damasy", age: 31 },
+      { id: 'asdqwe', name: "Mah.damasy", age: 30 }
+    ],
+    showPersons: false
+  };
+
+  switchNameHandler = newName => {
+    this.setState({
+      persons: [
+        { name: newName, age: 27 },
+        { name: "Doctor Damasy", age: 31 },
+        { name: "Mahmoud Damasy", age: 30 }
+      ]
+    });
+  };
+
+  nameChangeHandler = (event, id) => {
+    const persons = [...this.state.persons];
+
+    persons.map(p => {
+      if(p.id === id) p.name = event.target.value;
+      return p;
+    })
+    
+    this.setState({
+      persons: persons});
+  };
+
+  showPersonsHandler = () => {
+    this.setState({showPersons: !this.state.showPersons})
+  }
+
+  deletePersonHandler = (index) => {
+    const persons = [...this.state.persons];
+    persons.splice(index, 1);
+		this.setState({persons: persons});
+	};
+
+  render() {
+    const style = {
+      backgroundColor: "#61DAFB",
+      font: "16px",
+      color: "#000",
+      border: "none",
+      boxShadow: "0 0 2px rgba(0,0,0, 0.4)",
+      height: "40px",
+      cursor: "pointer",
+      margin: "10px auto",
+      padding: "10px",
+      borderRadius: "4px"
+    };
+
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+            {this.state.persons.map((person, index) => {
+                return <Person
+										name={person.name}
+										age={person.age}
+										key={person.id}
+										delete={() => this.deletePersonHandler(index)}
+										handler={(event) => this.nameChangeHandler(event, person.id)}
+									>
+									</Person>
+            })}
+        </div>
+      );
+    }
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <button style={style} onClick={this.showPersonsHandler}>
+            Toggle Persons
+          </button>
+
+          {persons}
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './Person.css'
+
+const person = params => {
+  return (
+    <div className="Person">
+      <p onClick={params.delete}>
+        I'm {params.name} ! and I'm {params.age} years old, {params.children}
+      </p>
+      <input type="text" onChange={params.handler} value={params.name}/>
+    </div>
+  );
+};
+
+export default person;
